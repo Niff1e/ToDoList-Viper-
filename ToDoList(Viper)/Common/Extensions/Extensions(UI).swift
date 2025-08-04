60e07fa991a94b9ada3470ec599ea8f4c6bd3c40
Niff1e <malanka.kering@gmail.com>
2025-08-05 00:59:12 +0300

    1. Minimal iOS version changed to 16.0. 2. Extensions(UI) did changed because of uncorrect version of strikeThrough function

--- ToDoList(Viper)/Common/Extensions/Extensions(UI).swift
+++ ToDoList(Viper)/Common/Extensions/Extensions(UI).swift
@@ -11,13 +11,16 @@ import UIKit
 extension UILabel {
     func strikeThrough(_ isStrikeThrough: Bool) {
         guard let text = self.text else { return }
-        let attributeString = NSMutableAttributedString(string: text)
-        let range = NSMakeRange(0, attributeString.length)
 
+        let attributeString = NSMutableAttributedString(string: text)
         if isStrikeThrough {
-            attributeString.addAttribute(.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: range)
+            attributeString.addAttribute(
+                .strikethroughStyle,
+                value: NSUnderlineStyle.single.rawValue,
+                range: NSMakeRange(0, attributeString.length)
+            )
         } else {
-            attributeString.removeAttribute(.strikethroughStyle, range: range)
+            attributeString.removeAttribute(.strikethroughStyle, range: NSMakeRange(0, attributeString.length))
         }
         self.attributedText = attributeString
     }
